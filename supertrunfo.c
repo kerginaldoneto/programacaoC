d4790e96f5df514257378734778aeb4ef77b0b63
kerginaldoneto <kerginaldorneto@gmail.com>
2025-05-12 13:54:10 -0300

    Update supertrunfo.c

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,8 +3,8 @@
 int main() {
     // Variáveis primeira carta
     char estado1;
-    char codigo1[4];
-    char nomeCidade1[50];
+    char codigo1[3];
+    char nomeCidade1[20];
     int populacao1;
     float area1;
     float pib1;
@@ -12,8 +12,8 @@ int main() {
     
     // Variáveis segunda carta
     char estado2;
-    char codigo2[4];
-    char nomeCidade2[50];
+    char codigo2[3];
+    char nomeCidade2[20];
     int populacao2;
     float area2;
     float pib2;
